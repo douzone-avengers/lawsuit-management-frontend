148629db31592e5c17f1ad99a9a419f9593a32ff
박지혁 <lhseqrhs@gmail.com>
2023-07-13 12:58:28 +0900

    애플리케이션 라우팅
    도메인(고객, 사건, 사원)별로 페이지를 나눴다

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
+import { BrowserRouter } from "react-router-dom";
 import { RecoilRoot } from "recoil";
+import AppRoutes from "./AppRoutes";
 
 export default function App() {
   return (
     <RecoilRoot>
-      <></>
+      <BrowserRouter>
+        <AppRoutes />
+      </BrowserRouter>
     </RecoilRoot>
   );
 }
