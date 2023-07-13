8d5af0d559442d8a1a2af45a4dff80fcdd523272
박지혁 <lhseqrhs@gmail.com>
2023-07-13 13:57:00 +0900

    카드 리스트 컴포넌트 추가
    도메인의 탭에 사용할 예정

---
+++ src/component/Card.tsx
@@ -0,0 +1,38 @@
+export type CardItem = {
+  id: number;
+  name: string;
+  checked: boolean;
+  onClick: () => void | Promise<void>;
+};
+
+type Props = {
+  card: CardItem;
+};
+
+export default function Card({ card }: Props) {
+  return (
+    <div
+      style={{
+        borderRadius: 20,
+        background: card.checked ? "black" : "white",
+        color: card.checked ? "white" : "black",
+      }}
+      onClick={card.onClick}
+    >
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          marginLeft: 24,
+          height: 48,
+          fontSize: 18,
+          fontWeight: 700,
+          fontFamily: "nato",
+          whiteSpace: "nowrap",
+        }}
+      >
+        {card.name}
+      </div>
+    </div>
+  );
+}
