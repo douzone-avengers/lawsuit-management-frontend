487622f77e9dd841e48ddabc0e8d8a400b148934
박지혁 <lhseqrhs@gmail.com>
2023-07-13 13:22:17 +0900

    로고 추가
    헤더에 홈으로 갈 수 있는 로고를 추가

--- src/component/Header.tsx
+++ src/component/Header.tsx
@@ -1,3 +1,27 @@
+import { useNavigate } from "react-router-dom";
+import Logo from "./Logo";
+
 export default function Header() {
-  return <div>TODO</div>;
+  const navigate = useNavigate();
+
+  return (
+    <div
+      style={{
+        borderBottom: "1px solid black",
+        display: "flex",
+        flexDirection: "row",
+        width: "100%",
+        height: "100%",
+        justifyContent: "space-between",
+      }}
+    >
+      <div
+        onClick={() => {
+          navigate("/");
+        }}
+      >
+        <Logo width={96} height={64} />
+      </div>
+    </div>
+  );
 }
