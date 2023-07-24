0d26f10deb9e6cfe712322f2e19f767387b94fbb
박지혁 <lhseqrhs@gmail.com>
2023-07-24 15:48:46 +0900

    feat: 접수 mock 서버 추가

--- src/components/case/CaseReception.tsx
+++ src/components/case/CaseReception.tsx
@@ -2,8 +2,30 @@ import Card from "@mui/material/Card";
 
 function CaseReception() {
   return (
-    <Card>
-      <div>
+    <>
+      <Card>
+        <div style={{ display: "flex" }}>
+          <div>기일 정보</div>
+          <button>등록</button>
+        </div>
+        <div>
+          <div>
+            <span>접수1</span>
+            <button>수정</button>
+            <button>삭제</button>
+          </div>
+          <div>
+            <span>접수2</span>
+            <button>수정</button>
+            <button>삭제</button>
+          </div>
+        </div>
+      </Card>
+      <Card>
+        <div style={{ display: "flex" }}>
+          <div>불변 기간</div>
+          <button>등록</button>
+        </div>
         <div>
           접수1 <button>수정</button>
           <button>삭제</button>
@@ -12,9 +34,8 @@ function CaseReception() {
           접수2 <button>수정</button>
           <button>삭제</button>
         </div>
-      </div>
-      <button>등록</button>
-    </Card>
+      </Card>
+    </>
   );
 }
 
---
+++ src/mock/reception/receptionHandlers.ts
@@ -0,0 +1,157 @@
+import { rest } from "msw";
+import receptionTable, { ReceptionType } from "./receptionTable.ts";
+
+const getReceptionsHandler = rest.get(
+  "/api/receptions",
+  async (req, res, ctx) => {
+    const lawsuitId = Number.parseInt(
+      req.url.searchParams.get("lawsuit") ?? "",
+    );
+    if (Number.isNaN(lawsuitId)) {
+      return res(ctx.status(400));
+    }
+
+    const data = receptionTable.filter(
+      (item) => item.lawsuitId === lawsuitId && !item.isDeleted,
+    );
+
+    return res(
+      ctx.status(200),
+      ctx.json({
+        data,
+      }),
+    );
+  },
+);
+
+const getDetailReceptionHandler = rest.get(
+  "/api/receptions/:receptionId",
+  async (req, res, ctx) => {
+    const receptionId = Number.parseInt(
+      (req.params["receptionId"] as string) ?? "",
+    );
+
+    if (Number.isNaN(receptionId)) {
+      return res(ctx.status(400));
+    }
+
+    const data = receptionTable.filter(
+      (item) => item.id === receptionId && !item.isDeleted,
+    )[0];
+
+    return res(
+      ctx.status(200),
+      ctx.json({
+        data,
+      }),
+    );
+  },
+);
+
+const postReceptionHandler = rest.post(
+  "/api/receptions",
+  async (req, res, ctx) => {
+    const body: {
+      lawsuitId: number;
+      isDone: boolean;
+      receptionType: ReceptionType;
+      contents: string;
+      receivedAt: string;
+      deadline: string;
+    } = await req.json();
+
+    receptionTable.push({
+      id: receptionTable.length + 1,
+      ...body,
+      receivedAt: new Date(body.receivedAt),
+      deadline: new Date(body.deadline),
+      isDeleted: false,
+    });
+
+    return res(ctx.status(200));
+  },
+);
+
+const updateReceptionHandler = rest.put(
+  "/api/receptions/update/:receptionId",
+  async (req, res, ctx) => {
+    const body: {
+      isDone: boolean;
+      receptionType: ReceptionType;
+      contents: string;
+      receivedAt: string;
+      deadline: string;
+    } = await req.json();
+
+    const receptionId = Number.parseInt(
+      (req.params["receptionId"] as string) ?? "",
+    );
+
+    if (Number.isNaN(receptionId)) {
+      return res(ctx.status(400));
+    }
+
+    const foundReception = receptionTable.filter(
+      (item) => item.id === receptionId && !item.isDeleted,
+    )[0];
+
+    if (!foundReception) {
+      return res(ctx.status(400));
+    }
+
+    if (body["isDone"] !== undefined) {
+      foundReception.isDone = body["isDone"];
+    }
+
+    if (body["receptionType"] !== undefined) {
+      foundReception.receptionType = body["receptionType"];
+    }
+
+    if (body["contents"] !== undefined) {
+      foundReception.contents = body["contents"];
+    }
+
+    if (body["receivedAt"] !== undefined) {
+      foundReception.receivedAt = new Date(body["receivedAt"]);
+    }
+
+    if (body["deadline"] !== undefined) {
+      foundReception.deadline = new Date(body["deadline"]);
+    }
+  },
+);
+
+const deleteReceptionHandler = rest.put(
+  "/api/receptions/delete/:receptionId",
+  async (req, res, ctx) => {
+    const receptionId = Number.parseInt(
+      (req.params["receptionId"] as string) ?? "",
+    );
+
+    if (Number.isNaN(receptionId)) {
+      return res(ctx.status(400));
+    }
+
+    const foundReception = receptionTable.filter(
+      (item) => item.id === receptionId && !item.isDeleted,
+    )[0];
+
+    if (!foundReception) {
+      return res(ctx.status(400));
+    }
+
+    foundReception.isDeleted = true;
+
+    return res(ctx.status(200));
+  },
+);
+
+const receptionHandler = [
+  getReceptionsHandler,
+  getDetailReceptionHandler,
+  postReceptionHandler,
+  updateReceptionHandler,
+  deleteReceptionHandler,
+];
+
+export default receptionHandler;
