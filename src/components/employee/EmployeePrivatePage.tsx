33c0a33b7da165c879287650a93841a8d18a98e6
ksj2083 <ksj2083@naver.com>
2023-07-25 16:47:01 +0900

    feat : 개인정보 페이지 기본화면 구현

--- src/components/employee/EmployeePrivatePage.tsx
+++ src/components/employee/EmployeePrivatePage.tsx
@@ -1,5 +1,7 @@
+import EmployeePrivateCard from "./EmployeePrivateCard";
+
 function EmployeePrivatePage() {
-  return <div>TODO: EmployeePrivatePage</div>;
+  return <EmployeePrivateCard />;
 }
 
 export default EmployeePrivatePage;
