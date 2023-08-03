65e22238740bd3a32a2958ac7726ba0d652fe916
한상진 <tkdwls6044@gmail.com>
2023-08-03 13:56:01 +0900

    feat: 차트 데이터 동적 반영

--- src/components/client/statistics/ClientCaseStatisticsChart.tsx
+++ src/components/client/statistics/ClientCaseStatisticsChart.tsx
@@ -6,7 +6,6 @@ import clientIdState from "../../../states/client/ClientIdState.tsx";
 import { LawsuitData } from "../../../mock/lawsuit/lawsuitTable.ts";
 import request, { RequestSuccessHandler } from "../../../lib/request.ts";
 import Card from "@mui/material/Card";
-import { produce } from "immer";
 
 type EChartsOption = echarts.EChartsOption;
 
@@ -15,106 +14,22 @@ function ClientCaseStatisticsChart() {
   const [cases, setCases] = useState<LawsuitData[]>([]);
   const totalAmountByStatusChartRef = useRef<HTMLDivElement>(null);
   const unreceivedAmountChartRef = useRef<HTMLDivElement>(null);
-  // 의뢰비 차트 옵션 설정
-  const [optionA, setOptionA] = useState<EChartsOption>({
-    title: {
-      text: "사건 상태별 총 금액",
-      left: "center",
-    },
-    grid: {
-      width: "50%",
-    },
-    tooltip: {
-      trigger: "item",
-    },
-    legend: {
-      orient: "vertical",
-      left: "left",
-    },
-    series: [
-      {
-        name: "상세 금액",
-        type: "pie",
-        radius: "70%",
-        data: [
-          {
-            value: 0,
-            name: "접수중",
-            itemStyle: { color: "#BACDF4" },
-          },
-          {
-            value: 0,
-            name: "진행중",
-            itemStyle: { color: "#59B0F7" },
-          },
-          {
-            value: 0,
-            name: "종결",
-            itemStyle: { color: "#5B73C9" },
-          },
-        ],
-        emphasis: {
-          itemStyle: {
-            shadowBlur: 10,
-            shadowOffsetX: 0,
-            shadowColor: "rgba(0, 0, 0, 0.5)",
-          },
-        },
-      },
-    ],
-  });
-  // 성공보수 차트 옵션 설정
-  const [optionB, setOptionB] = useState<EChartsOption>({
-    title: {
-      text: "미수령 금액",
-      left: "center",
-    },
-    grid: {
-      width: "50%",
-    },
-    tooltip: {
-      trigger: "item",
-    },
-    legend: {
-      orient: "vertical",
-      left: "left",
-    },
-    series: [
-      {
-        name: "상세 금액",
-        type: "pie",
-        radius: "70%",
-        data: [
-          {
-            value: 0,
-            name: "진행전 의뢰비",
-            itemStyle: { color: "#59B0F7" },
-          },
-          {
-            value: 0,
-            name: "기대 성공 보수",
-            itemStyle: { color: "#5B73C9" },
-          },
-        ],
-        emphasis: {
-          itemStyle: {
-            shadowBlur: 10,
-            shadowOffsetX: 0,
-            shadowColor: "rgba(0, 0, 0, 0.5)",
-          },
-        },
-      },
-    ],
-  });
+
+  const [valueA, setValueA] = useState(0); // totalAmountByStatus("등록")
+  const [valueB, setValueB] = useState(0); // totalAmountByStatus("진행")
+  const [valueC, setValueC] = useState(0); //totalAmountByStatus("종결")
 
   useEffect(() => {
     if (typeof memberId !== "number") {
       // TODO
       return;
     }
+
     const handleRequestSuccess: RequestSuccessHandler = (res) => {
       const body: { data: LawsuitData[] } = res.data;
       const { data } = body;
+      console.log("request success");
+
       setCases(data);
     };
 
@@ -124,18 +39,9 @@ function ClientCaseStatisticsChart() {
   }, [memberId]);
 
   useEffect(() => {
-    const newOptionA = produce(optionA, (draft: any) => {
-      draft.series[0].data[0].value = totalAmountByStatus("등록");
-      draft.series[0].data[1].value = totalAmountByStatus("진행");
-      draft.series[0].data[2].value = totalAmountByStatus("종결");
-    });
-    setOptionA(newOptionA);
-
-    const newOptionB = produce(optionB, (draft: any) => {
-      draft.series[0].data[0].value = unreceivedAmountByStatus("등록");
-      draft.series[0].data[1].value = calculateSuccessAmount();
-    });
-    setOptionB(newOptionB);
+    setValueA(totalAmountByStatus("등록"));
+    setValueB(totalAmountByStatus("진행"));
+    setValueC(totalAmountByStatus("종결"));
   }, [cases]);
 
   useEffect(() => {
@@ -146,10 +52,103 @@ function ClientCaseStatisticsChart() {
       unreceivedAmountChartRef.current,
     );
 
+    // 의뢰비 차트 옵션 설정
+    const totalAmountByStatusChartOptions: EChartsOption = {
+      title: {
+        text: "사건 상태별 총 금액",
+        left: "center",
+      },
+      grid: {
+        width: "50%",
+      },
+      tooltip: {
+        trigger: "item",
+      },
+      legend: {
+        orient: "vertical",
+        left: "left",
+      },
+      series: [
+        {
+          name: "상세 금액",
+          type: "pie",
+          radius: "70%",
+          data: [
+            {
+              value: valueA,
+              name: "접수중",
+              itemStyle: { color: "#BACDF4" },
+            },
+            {
+              value: valueB,
+              name: "진행중",
+              itemStyle: { color: "#59B0F7" },
+            },
+            {
+              value: valueC,
+              name: "종결",
+              itemStyle: { color: "#5B73C9" },
+            },
+          ],
+          emphasis: {
+            itemStyle: {
+              shadowBlur: 10,
+              shadowOffsetX: 0,
+              shadowColor: "rgba(0, 0, 0, 0.5)",
+            },
+          },
+        },
+      ],
+    };
+
+    // 성공보수 차트 옵션 설정
+    const unreceivedAmountChartOptions: EChartsOption = {
+      title: {
+        text: "미수령 금액",
+        left: "center",
+      },
+      grid: {
+        width: "50%",
+      },
+      tooltip: {
+        trigger: "item",
+      },
+      legend: {
+        orient: "vertical",
+        left: "left",
+      },
+      series: [
+        {
+          name: "상세 금액",
+          type: "pie",
+          radius: "70%",
+          data: [
+            {
+              value: unreceivedAmountByStatus("등록"),
+              name: "진행전 의뢰비",
+              itemStyle: { color: "#59B0F7" },
+            },
+            {
+              value: calculateSuccessAmount(),
+              name: "기대 성공 보수",
+              itemStyle: { color: "#5B73C9" },
+            },
+          ],
+          emphasis: {
+            itemStyle: {
+              shadowBlur: 10,
+              shadowOffsetX: 0,
+              shadowColor: "rgba(0, 0, 0, 0.5)",
+            },
+          },
+        },
+      ],
+    };
+
     // @ts-ignore
-    totalAmountByStatusChart.setOption(optionA);
+    totalAmountByStatusChart.setOption(totalAmountByStatusChartOptions);
     // @ts-ignore
-    unreceivedAmountChart.setOption(optionB);
+    unreceivedAmountChart.setOption(unreceivedAmountChartOptions);
 
     // 창 크기 조절 이벤트 핸들러 추가
     const handleResize = () => {
@@ -162,8 +161,9 @@ function ClientCaseStatisticsChart() {
     return () => {
       window.removeEventListener("resize", handleResize);
     };
-  }, []);
+  }, [valueA, valueB, valueC]);
 
+  // cases 데이터를 비동기로 처리해야함..
   function totalAmountByStatus(status: string): number {
     const total = cases.reduce((acc, item) => {
       return item.lawsuitStatus === status ? acc + item.commissionFee : acc;
@@ -187,6 +187,8 @@ function ClientCaseStatisticsChart() {
 
     return total;
   }
+
+  console.log("render");
 
   return (
     <Box
