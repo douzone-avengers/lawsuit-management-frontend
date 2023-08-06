87425d58ddb9626ef3f7c6173ad4533f9818a96b
ksj2083 <ksj2083@naver.com>
2023-08-06 22:40:42 +0900

    feat : 직원 회원가입 구현시작

--- src/components/join/JoinPage.tsx
+++ src/components/join/JoinPage.tsx
@@ -2,13 +2,14 @@ import Box from "@mui/material/Box";
 import { useRecoilValue } from "recoil";
 import validatedClientState from "../../states/join/ValidatedClientState";
 import ClientJoinPage from "./ClientJoinPage";
+import EmployeeJoinPage from "./EmployeeJoinPage";
 
 function JoinPage() {
   const validatedClient = useRecoilValue(validatedClientState);
 
   return (
     <Box>
-      validatedClient ? <ClientJoinPage /> : null
+      validatedClient ? <ClientJoinPage /> : <EmployeeJoinPage />
     </Box>
   );
 }
