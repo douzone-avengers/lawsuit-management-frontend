99d7a74cdeaba6b872473eda207626ad302aad6f
한상진 <tkdwls6044@gmail.com>
2023-08-13 14:12:05 +0900

    fix: 의뢰인-사건정보 탭 페이징

--- src/components/client/ClientCaseListTab.tsx
+++ src/components/client/ClientCaseListTab.tsx
@@ -1,42 +1,60 @@
 import Box from "@mui/material/Box";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { useRecoilValue } from "recoil";
+import { useRecoilValue, useSetRecoilState } from "recoil";
 import request, { RequestSuccessHandler } from "../../lib/request";
 import clientIdState from "../../states/client/ClientIdState";
 import CaseListTable from "../case/CaseListTable.tsx";
 import { LawsuitInfo } from "../case/type/LawsuitInfo.tsx";
-import { PagingInfo } from "../common/type/PagingInfo.tsx";
+import lawsuitsPageState from "../../states/case/info/lawsuitsPaging/LawsuitsPageState.tsx";
 
 function ClientCaseListTab() {
   const clientId = useRecoilValue(clientIdState);
   const navigate = useNavigate();
   const [cases, setCases] = useState<LawsuitInfo[]>([]);
 
+  //for paging
+  const page = useRecoilValue(lawsuitsPageState);
+  const setPage = useSetRecoilState(lawsuitsPageState);
+  const [rowsPerPage, setRowsPerPage] = useState(5);
+  const [count, setCount] = useState(0);
+
+  const prevDependencies = useRef({
+    rowsPerPage,
+    page,
+  });
+
   useEffect(() => {
     if (typeof clientId !== "number") {
       // TODO
       return;
     }
+
+    prevDependencies.current = {
+      rowsPerPage,
+      page,
+    };
+
     const handleRequestSuccess: RequestSuccessHandler = (res) => {
       const lawsuitData: {
         lawsuitList: LawsuitInfo[];
-        pageRange: PagingInfo;
+        count: number;
       } = res.data;
-      console.log(lawsuitData);
+
       setCases(lawsuitData.lawsuitList);
+      setCount(lawsuitData.count);
     };
 
     request("GET", `/lawsuits/clients/${clientId}`, {
       useMock: false,
       withToken: true,
       params: {
-        curPage: "1",
-        itemsPerPage: "10",
+        curPage: (page + 1).toString(),
+        rowsPerPage: rowsPerPage.toString(),
       },
       onSuccess: handleRequestSuccess,
     });
-  }, [clientId]);
+  }, [clientId, rowsPerPage, page]);
 
   return (
     <Box>
@@ -47,6 +65,11 @@ function ClientCaseListTab() {
             navigate(`/cases/${item.id}?client=${clientId}`);
           },
         }))}
+        count={count}
+        page={page}
+        setPage={setPage}
+        rowsPerPage={rowsPerPage}
+        setRowsPerPage={setRowsPerPage}
       />
     </Box>
   );
--- src/components/common/type/PagingInfo.tsx
+++
@@ -1,4 +0,0 @@
-export type PagingInfo = {
-  startPage: number;
-  endPage: number;
-};
