07dd482e37e2544b63a60d006b45e18f70ecd2e9
ksj2083 <ksj2083@naver.com>
2023-09-14 22:47:35 +0900

    https

--- src/components/chat/socket.ts
+++ src/components/chat/socket.ts
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 
 const socket = io(
-  import.meta.env.DEV ? "http://localhost:7777" : "http://52.79.44.141:7777",
+  import.meta.env.DEV ? "http://localhost:7777" : "https://52.79.44.141:7777",
 );
 
 export default socket;
