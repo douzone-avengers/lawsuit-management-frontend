b59bf194df634f6c6d1ed418fb864d09cc9c00f2
ksj2083 <ksj2083@naver.com>
2023-09-15 04:21:30 +0900

    소켓 테스트2

--- src/components/chat/socket.ts
+++ src/components/chat/socket.ts
@@ -3,7 +3,7 @@ import { io } from "socket.io-client";
 const socket = io(
   import.meta.env.DEV
     ? "http://localhost:7777"
-    : "https://lawsuit-management-api.store",
+    : "https://lawsuit-management-api.store:7777",
 );
 
 export default socket;
